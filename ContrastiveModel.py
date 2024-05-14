b514dc3e45edc0f9930f529776ff96435d08ca56
kamil.tokarek_263615 <263615@student.pwr.edu.pl>
2024-05-14 15:21:21 +0200

    refactor contrastive loss model

---
+++ ContrastiveModel.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+def create_model(img_shape):
+    input = tf.keras.layers.Input(shape=img_shape)
+    x = tf.keras.layers.BatchNormalization()(input)
+    x = tf.keras.layers.Conv2D(4, (5, 5), activation="tanh")(x)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+    x = tf.keras.layers.Conv2D(16, (5, 5), activation="tanh")(x)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+    x = tf.keras.layers.Flatten()(x)
+
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dense(100, activation="tanh")(x)
+    embedding_network = tf.keras.Model(input, x)
+
+    return embedding_network
